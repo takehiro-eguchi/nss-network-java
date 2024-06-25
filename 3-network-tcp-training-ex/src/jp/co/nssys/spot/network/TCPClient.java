34032912b2a9e7d02d6a218a7c729acd44bae2e5
t-eguchi <t-eguchi@nssys.co.jp>
2024-06-25 18:48:47 +0900

    応用編のTCP演習を追加

---
+++ 3-network-tcp-training-ex/src/jp/co/nssys/spot/network/TCPClient.java
@@ -0,0 +1,61 @@
+package jp.co.nssys.spot.network;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+/**
+ * TCP通信におけるクライアントクラスです。
+ */
+public class TCPClient {
+
+	/**
+	 * 実行します。
+	 * @param args
+	 */
+	public static void main(String[] args) throws IOException {
+		// ソケットの作成
+		try (var socket = new Socket("localhost", 8080)) {
+			try (
+					var consoleStreamReader = new InputStreamReader(System.in);
+					var consoleReader = new BufferedReader(consoleStreamReader);
+					var sockInputStream = socket.getInputStream();
+					var sockInputStreamReader = new InputStreamReader(sockInputStream);
+					var sockReader = new BufferedReader(sockInputStreamReader);
+					var sockOutputStream = socket.getOutputStream();
+					var sockOutputStreamWriter = new OutputStreamWriter(sockOutputStream);
+					var sockWriter = new BufferedWriter(sockOutputStreamWriter);) {
+				
+				while (true) {					
+					// 標準入力から読み込む
+					System.out.println("メッセージの送信をやめる場合はエンターを入力してください。");
+					System.out.print("メッセージ：");
+					var msg = consoleReader.readLine();
+					if (msg == null || msg.isEmpty()) {
+						break;
+					}
+
+					// 送信する
+					sockWriter.write(msg);
+					sockWriter.newLine();
+					sockWriter.flush();
+
+					// 結果を受信する
+					var res = sockReader.readLine();
+					System.out.println("結果を受け取りました:" + res);
+				}
+
+				// シャットダウン
+				System.out.println("メッセージの送信を終了します。");
+				socket.shutdownInput();
+				socket.shutdownOutput();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ 3-network-tcp-training-ex/src/jp/co/nssys/spot/network/TCPClient2.java
@@ -0,0 +1,14 @@
+package jp.co.nssys.spot.network;
+
+import java.io.IOException;
+
+/**
+ * 2台目のクライアント
+ */
+public class TCPClient2 {
+
+	public static void main(String[] args) throws IOException {
+		// 委譲
+		TCPClient.main(args);
+	}
+}
